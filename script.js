02d297f3a9e0dacb922623f4f9becf9100534132
robovmp <mpviniciusmp@gmail.com>
2022-01-24 11:13:29 -0300

    desenvovendo o js

---
+++ script.js
@@ -0,0 +1,4 @@
+var input1 = document.querySelector("#supEsq");
+var input2 = document.querySelector("#supDir");
+var input3 = document.querySelector("#infDir");
+var input4 = document.querySelector("#infEsq");
